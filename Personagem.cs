1ff40b426acf21819d57ba8bac00fa09c41a9086
unknown <g.hitoshi.yokogawa@gmail.com>
2020-05-22 09:56:04 -0300

    Primeiro POO

---
+++ Personagem.cs
@@ -0,0 +1,15 @@
+namespace PrimeiroPOO
+{
+    public class Personagem
+    {
+        public string nome = "Jake Long";
+        public int idade = 13;
+        public string dragon;
+        public string use;
+
+        public string Atacar()
+        {
+            return "O personagem atacou de maneira espetacular!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem jake = new Personagem();
+            jake.dragon = "Dragon form!!";
+            jake.use = "Jake Long usa...";
+
+            Console.WriteLine(jake.nome);
+
+            Console.WriteLine(jake.idade);
+
+            Console.WriteLine(jake.use);
+
+            Console.WriteLine(jake.dragon);
+
+            Console.WriteLine(jake.Atacar());
+
+        }
+    }
+}
